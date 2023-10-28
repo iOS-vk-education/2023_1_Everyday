15764866a1aad10c18755aef6c9c7694adefdc7d
fix1in1it <fix1in1it@gmail.com>
2023-10-28 10:51:09 +0300

    app initialized

---
+++ Everyday/Everyday/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Everyday
+//
+//  Created by user on 28.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green  // to check if everything is fine
+    }
+}
