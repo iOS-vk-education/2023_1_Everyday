d831a8328dcac4c849c8034c16c1b84bd14897ac
fix1in1it <fix1in1it@gmail.com>
2023-10-31 16:57:14 +0300

    add tabbar+screens

---
+++ Everyday/Everyday/Screens/LoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginVC.swift
+//  Everyday
+//
+//  Created by user on 31.10.2023.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
