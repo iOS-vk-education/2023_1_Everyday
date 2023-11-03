21024346093fc00a7e4ed182305f5e656e94dfde
fix1in1it <fix1in1it@gmail.com>
2023-11-03 14:11:51 +0300

    columns chart

--- Everyday/Everyday/Screens/ViewController.swift
+++ Everyday/Everyday/Screens/ViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green  // to check if everything is fine
+        setUpView()
+    }
+    
+    func setUpView() {
+        let controller = UIHostingController(rootView: ContentView())
+        
+        guard let chartView = controller.view else { return }
+        
+        view.addSubview(chartView)
+        
+        chartView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            // set constraints
+        ])
     }
 }
