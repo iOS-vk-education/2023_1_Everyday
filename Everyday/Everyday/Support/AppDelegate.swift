6da732cd76ab91326a501281fd9e062007e32d34
fix1in1it <fix1in1it@gmail.com>
2023-11-04 19:57:32 +0300

    Revert "attempt to connect linter"
    This reverts commit 7b71ba612686dd59b1e045ab6dc8df5a4cf0c240.

--- Everyday/Everyday/Support/AppDelegate.swift
+++ Everyday/Everyday/Support/AppDelegate.swift
@@ -10,14 +10,14 @@ import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         return true
     }
-    
+
     // MARK: UISceneSession Lifecycle
-    
+
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
