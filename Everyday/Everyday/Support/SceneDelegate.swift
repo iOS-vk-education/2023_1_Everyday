313274d21d8291b0affc09d3c23d407985790f71
MX126 <mikekhristokyan@gmail.com>
2023-11-09 21:19:45 +0300

    no crit view

--- Everyday/Everyday/Support/SceneDelegate.swift
+++ Everyday/Everyday/Support/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
         
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = EDTabBarController()
+        window?.rootViewController = AuthScreenVC()
         window?.makeKeyAndVisible()
     }
 }
