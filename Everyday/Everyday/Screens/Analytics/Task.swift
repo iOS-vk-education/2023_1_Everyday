97252d5e472bac68edc0aa83be63299e610896a6
fix1in1it <fix1in1it@gmail.com>
2023-11-12 16:12:08 +0300

    future task model

--- Everyday/Everyday/Screens/Analytics/Task.swift
+++ Everyday/Everyday/Screens/Analytics/Task.swift
@@ -1,27 +1,24 @@
-//
-//  Task.swift
-//  Everyday
-//
-//  Created by user on 10.11.2023.
-//
-
 import Foundation
 
-enum Priority: String {
-    case high   = "Important"
-    case medium = "Urgent"
-    case low    = "Low prio"
-    case none   = "Unnecessary"
-}
-
-struct Tag {
+struct GridCell: Identifiable {
+    let id = UUID()
     let name: String
+    var chartType: ChartType
 }
 
-struct Task {
-    var title: String
-    var priority: Priority = .none
-    var tags: [Tag]
+enum Priority: Int {
+    case high   = 0  // "Important"
+    case medium = 1  // "Urgent"
+    case low    = 2  // "Low prio"
+    case none   = 3  // "Unnecessary"
+}
+
+enum ChartType: String, CaseIterable, Identifiable {
+    case bar    = "Bar"
+    case line   = "Line"
+    var id: String {
+        self.rawValue
+    }
 }
 
 struct ViewMonth: Identifiable, Hashable {
@@ -40,6 +37,14 @@ extension Date {
 
 struct MockData {
     
+    static let example = [ViewMonth(date: Date.from(year: 2023, month: 1, day: 1), viewCount: 5, priority: .high),
+                          ViewMonth(date: Date.from(year: 2023, month: 2, day: 1), viewCount: 6, priority: .high),
+                          ViewMonth(date: Date.from(year: 2023, month: 3, day: 1), viewCount: 2, priority: .high),
+                          ViewMonth(date: Date.from(year: 2023, month: 4, day: 1), viewCount: 11, priority: .high),
+                          ViewMonth(date: Date.from(year: 2023, month: 5, day: 1), viewCount: 3, priority: .high),
+                          ViewMonth(date: Date.from(year: 2023, month: 6, day: 1), viewCount: 5, priority: .high),
+                          ViewMonth(date: Date.from(year: 2023, month: 7, day: 1), viewCount: 10, priority: .high),]
+    
     static let viewMonth: [[ViewMonth]] = [
         [.init(date: Date.from(year: 2023, month: 1, day: 1), viewCount: 5, priority: .high),
         .init(date: Date.from(year: 2023, month: 2, day: 1), viewCount: 6, priority: .high),
@@ -49,21 +54,21 @@ struct MockData {
         .init(date: Date.from(year: 2023, month: 6, day: 1), viewCount: 5, priority: .high),
         .init(date: Date.from(year: 2023, month: 7, day: 1), viewCount: 10, priority: .high),],
         
-        [.init(date: Date.from(year: 2023, month: 1, day: 1), viewCount: 5, priority: .medium),
-        .init(date: Date.from(year: 2023, month: 2, day: 1), viewCount: 6, priority: .medium),
-        .init(date: Date.from(year: 2023, month: 3, day: 1), viewCount: 2, priority: .medium),
-        .init(date: Date.from(year: 2023, month: 4, day: 1), viewCount: 11, priority: .medium),
-        .init(date: Date.from(year: 2023, month: 5, day: 1), viewCount: 3, priority: .medium),
-        .init(date: Date.from(year: 2023, month: 6, day: 1), viewCount: 5, priority: .medium),
-        .init(date: Date.from(year: 2023, month: 7, day: 1), viewCount: 10, priority: .medium),],
+        [.init(date: Date.from(year: 2023, month: 1, day: 1), viewCount: 1, priority: .medium),
+        .init(date: Date.from(year: 2023, month: 2, day: 1), viewCount: 2, priority: .medium),
+        .init(date: Date.from(year: 2023, month: 3, day: 1), viewCount: 3, priority: .medium),
+        .init(date: Date.from(year: 2023, month: 4, day: 1), viewCount: 4, priority: .medium),
+        .init(date: Date.from(year: 2023, month: 5, day: 1), viewCount: 5, priority: .medium),
+        .init(date: Date.from(year: 2023, month: 6, day: 1), viewCount: 6, priority: .medium),
+        .init(date: Date.from(year: 2023, month: 7, day: 1), viewCount: 7, priority: .medium),],
         
-        [.init(date: Date.from(year: 2023, month: 1, day: 1), viewCount: 5, priority: .low),
+        [.init(date: Date.from(year: 2023, month: 1, day: 1), viewCount: 7, priority: .low),
         .init(date: Date.from(year: 2023, month: 2, day: 1), viewCount: 6, priority: .low),
-        .init(date: Date.from(year: 2023, month: 3, day: 1), viewCount: 2, priority: .low),
-        .init(date: Date.from(year: 2023, month: 4, day: 1), viewCount: 11, priority: .low),
+        .init(date: Date.from(year: 2023, month: 3, day: 1), viewCount: 5, priority: .low),
+        .init(date: Date.from(year: 2023, month: 4, day: 1), viewCount: 4, priority: .low),
         .init(date: Date.from(year: 2023, month: 5, day: 1), viewCount: 3, priority: .low),
-        .init(date: Date.from(year: 2023, month: 6, day: 1), viewCount: 5, priority: .low),
-        .init(date: Date.from(year: 2023, month: 7, day: 1), viewCount: 10, priority: .low),],
+        .init(date: Date.from(year: 2023, month: 6, day: 1), viewCount: 2, priority: .low),
+        .init(date: Date.from(year: 2023, month: 7, day: 1), viewCount: 1, priority: .low),],
         
         [.init(date: Date.from(year: 2023, month: 1, day: 1), viewCount: 5, priority: .none),
         .init(date: Date.from(year: 2023, month: 2, day: 1), viewCount: 6, priority: .none),
