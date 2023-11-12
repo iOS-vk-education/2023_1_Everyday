6eb2ddc38c2d9c21e1882955faadf344db45a334
fix1in1it <fix1in1it@gmail.com>
2023-11-12 15:42:42 +0300

    view model for grid

--- Everyday/Everyday/Screens/Analytics/ChartGridViewModel.swift
+++ Everyday/Everyday/Screens/Analytics/ChartGridViewModel.swift
@@ -5,4 +5,21 @@
 //  Created by user on 11.11.2023.
 //
 
-import Foundation
+import SwiftUI
+
+final class ChartGridViewModel: ObservableObject {
+    
+    var selectedChart: Priority? {
+        didSet {
+            isShowingDetailView = true
+        }
+    }
+    
+    @Published var isShowingDetailView = false
+    @Published var chartTypes: [GridCell] = [
+        GridCell(name: "Important", chartType: .bar),
+        GridCell(name: "Urgent", chartType: .bar),
+        GridCell(name: "Low prio", chartType: .bar),
+        GridCell(name: "Unnecessary", chartType: .bar)
+    ]
+}
