2910da3703cc107706522704112375ce8ce84c0b
MX126 <mikekhristokyan@gmail.com>
2023-12-24 17:26:50 +0300

    optimized filter vc

--- Everyday/Everyday/Modules/Establishment/AddTask/Calendar/CoreSettings/FilterVC.swift
+++ Everyday/Everyday/Modules/Establishment/AddTask/Calendar/CoreSettings/FilterVC.swift
@@ -6,41 +6,55 @@
 //
 
 import UIKit
-import HorizonCalendar
 
 final class FilterVC: UIViewController {
-    
-// MARK: - Properties
 
-    var overlayView: UIView?
-    let checkmarkImageView = UIImageView(image: UIImage(systemName: "checkmark"))
-    var selectedFieldIndex = 0
-    static private var selectedButton: UIButton?
-    private var stackView = UIStackView()
-    static private var isFirstLoad = true
-    
+    // MARK: - Properties
+
+    private var overlayView: UIView?
+    private let checkmarkImageView = UIImageView(image: UIImage(systemName: "checkmark"))
+    private static var selectedButton: UIButton?
+    private static var isFirstLoad = true
+
     private var onUpdate: ((Int) -> Void)?
-    
+
     init(onUpdate: ((Int) -> Void)?) {
         super.init(nibName: nil, bundle: nil)
         self.onUpdate = onUpdate
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-// MARK: - Life cycle
-    
+
+    // MARK: - Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupOverlay()
+    }
 
+    // MARK: - Actions
+
+    @objc private func hideOverlay() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @objc private func openDifferentView(_ sender: UIButton) {
+        hideOverlay()
+        updateSelectedButton(sender)
+        onUpdate?(sender.tag)
+    }
+
+    // MARK: - Private methods
+
+    private func setupOverlay() {
         let overlaySize: CGFloat = 150
         let overlay = UIView(frame: CGRect(x: (view.frame.width - overlaySize) / 2,
-                                           y: (view.frame.height - overlaySize) / 2, width: overlaySize + 20, height: overlaySize))
+                                           y: (view.frame.height - overlaySize) / 2,
+                                           width: overlaySize + 20, height: overlaySize))
         overlay.backgroundColor = UIColor(named: "EverydayLightBlue")
         overlay.layer.cornerRadius = 10.0
-
         view.addSubview(overlay)
         overlayView = overlay
 
@@ -51,46 +65,15 @@ final class FilterVC: UIViewController {
         stackView.frame = overlay.bounds
 
         for i in 1...2 {
-            let button = UIButton(type: .system)
-            button.setTitleColor(UIColor.white, for: .normal)
-            if i == 1 {
-                button.setTitle("Выбрать день", for: .normal)
-            } else {
-                button.setTitle("Выбрать период", for: .normal)
-            }
-            button.addTarget(self, action: #selector(openDifferentView(_:)), for: .touchUpInside)
-            button.tag = i
+            let button = createButton(tag: i, title: (i == 1) ? "Выбрать день" : "Выбрать период")
             stackView.addArrangedSubview(button)
-            
-            if FilterVC.isFirstLoad {
-                let checkmarkImageView = UIImageView(image: UIImage(systemName: "checkmark"))
-                checkmarkImageView.tintColor = UIColor(named: "EverydayOrange")
-                button.addSubview(checkmarkImageView)
-                checkmarkImageView.translatesAutoresizingMaskIntoConstraints = false
-                NSLayoutConstraint.activate([
-                    checkmarkImageView.trailingAnchor.constraint(equalTo: button.trailingAnchor, constant: -10),
-                    checkmarkImageView.centerYAnchor.constraint(equalTo: button.centerYAnchor)
-                ])
-                FilterVC.isFirstLoad = false
-            }
 
-            if let selectedButton = FilterVC.selectedButton, selectedButton.tag == i {
-                let checkmarkImageView = UIImageView(image: UIImage(systemName: "checkmark"))
-                checkmarkImageView.tintColor = UIColor(named: "EverydayOrange")
-                button.addSubview(checkmarkImageView)
-                checkmarkImageView.translatesAutoresizingMaskIntoConstraints = false
-                NSLayoutConstraint.activate([
-                    checkmarkImageView.trailingAnchor.constraint(equalTo: button.trailingAnchor, constant: -10),
-                    checkmarkImageView.centerYAnchor.constraint(equalTo: button.centerYAnchor)
-                ])
+            if FilterVC.isFirstLoad || (FilterVC.selectedButton?.tag == i) {
+                setupCheckmark(for: button)
             }
 
             if i == 1 {
-                let separatorView = UIView()
-                separatorView.backgroundColor = UIColor.black
-                stackView.addArrangedSubview(separatorView)
-                separatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
-                separatorView.widthAnchor.constraint(equalTo: stackView.widthAnchor).isActive = true
+                stackView.addSeparator(color: .black)
             }
         }
 
@@ -99,16 +82,30 @@ final class FilterVC: UIViewController {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideOverlay))
         view.addGestureRecognizer(tapGesture)
     }
-    
-// MARK: - Actions
-    
-    @objc func hideOverlay() {
-        dismiss(animated: true, completion: nil)
+
+    private func createButton(tag: Int, title: String) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle(title, for: .normal)
+        button.tag = tag
+        button.addTarget(self, action: #selector(openDifferentView(_:)), for: .touchUpInside)
+        return button
     }
-    
-    @objc func openDifferentView(_ sender: UIButton) {
-        hideOverlay()
-        
+
+    private func setupCheckmark(for button: UIButton) {
+        let checkmarkImageView = UIImageView(image: UIImage(systemName: "checkmark"))
+        checkmarkImageView.tintColor = UIColor(named: "EverydayOrange")
+        button.addSubview(checkmarkImageView)
+        checkmarkImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            checkmarkImageView.trailingAnchor.constraint(equalTo: button.trailingAnchor, constant: -10),
+            checkmarkImageView.centerYAnchor.constraint(equalTo: button.centerYAnchor)
+        ])
+        FilterVC.isFirstLoad = false
+        FilterVC.selectedButton = button
+    }
+
+    private func updateSelectedButton(_ sender: UIButton) {
         FilterVC.selectedButton?.subviews.compactMap { $0 as? UIImageView }.forEach { $0.removeFromSuperview() }
 
         let checkmarkImageView = UIImageView(image: UIImage(systemName: "checkmark"))
@@ -117,16 +114,15 @@ final class FilterVC: UIViewController {
         checkmarkImageView.translatesAutoresizingMaskIntoConstraints = false
 
         FilterVC.selectedButton = sender
-                
-        if let title = sender.titleLabel?.text {
-            switch sender.tag {
-            case 1:
-                onUpdate?(1)
-            case 2:
-                onUpdate?(2)
-            default:
-                break
-            }
-        }
+    }
+}
+
+extension UIStackView {
+    func addSeparator(color: UIColor) {
+        let separatorView = UIView()
+        separatorView.backgroundColor = color
+        addArrangedSubview(separatorView)
+        separatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        separatorView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
     }
 }
